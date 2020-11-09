8d8ceea7d846f932c8167bd70b0d0548fcbbbdd2
infixman <infixman@gmail.com>
2020-11-10 01:20:21 +0800

    change bot to use polling

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
-from configparser import ConfigParser
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackContext)
+
+TOKEN = 'YOUR TOKEN HERE'
+
+
+def hello(update: Update, context: CallbackContext):
+    update.message.reply_text('hello {}'.format(update.message.from_user.name))
+
+
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.start_polling()
+updater.idle()
