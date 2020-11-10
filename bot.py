0bc4e732206a339211fede25372c170db9dfbc06
infixman <infixman@gmail.com>
2020-11-11 01:28:25 +0800

    add more and more log for debug on heroku

--- bot.py
+++ bot.py
@@ -129,8 +129,25 @@ def ask_max(update: Update, context: CallbackContext):
 
 @cache.memoize(ttl=3 * 60, typed=True)
 def get_max_price():
+    logger.info('get max price')
+    headers = {'authority': 'max.maicoin.com',
+               'pragma': 'no-cache',
+               'cache-control': 'no-cache',
+               'dnt': '1',
+               'upgrade-insecure-requests': '1',
+               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                         '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/86.0.4240.183 Safari/537.36',
+               'sec-fetch-site': 'none',
+               'sec-fetch-mode': 'navigate',
+               'sec-fetch-user': '?1',
+               'sec-fetch-dest': 'document',
+               'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7'}
     r = requests.get(
-        'https://max.maicoin.com/trades/usdttwd/recent_trades')
+        'https://max.maicoin.com/trades/usdttwd/recent_trades',
+        headers=headers)
+    logger.info('get body:{}'.format(r.text))
     obj = json.loads(r.text)
     logger.info(obj)
     if not obj['data']:
