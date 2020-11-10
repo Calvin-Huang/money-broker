1e42ef8b9aeada6e5687c8a7d0f5e5aebaa8fba3
Eason Cheng <eason.cheng@xuenn.com>
2020-11-10 17:24:12 +0800

    change telegram bot to webhook mode

--- bot.py
+++ bot.py
@@ -9,8 +9,9 @@ from telegram.ext import (
     CommandHandler,
     CallbackContext)
 
-
+APPNAME = os.environ["APPNAME"]
 TOKEN = os.environ["TOKEN"]
+PORT = int(os.environ.get('PORT', '8443'))
 cache = Cache()
 
 
@@ -22,16 +23,16 @@ def mastercardRate(update: Update, context: CallbackContext):
 @cache.memoize(ttl=10 * 60, typed=True)
 def getMastercardRate():
     headers = {'authority': 'www.mastercard.us',
-                  'pragma': 'no-cache',
-                  'cache-control': 'no-cache',
-                  'accept': 'application/json, text/plain, */*',
-                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                'Chrome/86.0.4240.183 Safari/537.36',
-                  'sec-fetch-site': 'same-origin',
-                  'sec-fetch-mode': 'cors',
-                  'sec-fetch-dest': 'empty',
-                  'referer': 'https://www.mastercard.us/en-us/personal/get-support/convert-currency.html',
-                  'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7'}
+               'pragma': 'no-cache',
+               'cache-control': 'no-cache',
+               'accept': 'application/json, text/plain, */*',
+               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/86.0.4240.183 Safari/537.36',
+               'sec-fetch-site': 'same-origin',
+               'sec-fetch-mode': 'cors',
+               'sec-fetch-dest': 'empty',
+               'referer': 'https://www.mastercard.us/en-us/personal/get-support/convert-currency.html',
+               'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7'}
     r = requests.get('https://www.mastercard.us/settlement/currencyrate/fxDate=0000-00-00;transCurr=USD;'
                      'crdhldBillCurr=TWD;bankFee=0;transAmt=1/conversion-rate',
                      headers=headers)
@@ -46,7 +47,8 @@ def visaRate(update: Update, context: CallbackContext):
 
 @cache.memoize(ttl=10 * 60, typed=True)
 def getVisaRate():
-    r = requests.get('https://www.visa.com.tw/travel-with-visa/exchange-rate-calculator.html?fromCurr=TWD&toCurr=USD&fee=0')
+    r = requests.get(
+        'https://www.visa.com.tw/travel-with-visa/exchange-rate-calculator.html?fromCurr=TWD&toCurr=USD&fee=0')
     soup = BeautifulSoup(r.text, 'html.parser')
     selector = 'span strong+ strong'
     rate = [i.text for i in soup.select(selector)][0]
@@ -55,7 +57,8 @@ def getVisaRate():
 
 @cache.memoize(ttl=10 * 60, typed=True)
 def rate(update: Update, context: CallbackContext):
-    update.message.reply_text('Mastercard: 1 USD = {} TWD\nVisa: 1 USD = {} TWD'.format(getMastercardRate(), getVisaRate()))
+    update.message.reply_text(
+        'Mastercard: 1 USD = {} TWD\nVisa: 1 USD = {} TWD'.format(getMastercardRate(), getVisaRate()))
 
 
 updater = Updater(TOKEN)
@@ -65,5 +68,9 @@ updater.dispatcher.add_handler(CommandHandler('v', visaRate))
 updater.dispatcher.add_handler(CommandHandler('visa', visaRate))
 updater.dispatcher.add_handler(CommandHandler('r', rate))
 updater.dispatcher.add_handler(CommandHandler('rate', rate))
-updater.start_polling()
+# updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(APPNAME, TOKEN))
 updater.idle()
