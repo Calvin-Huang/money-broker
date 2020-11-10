2f1338ba9df6abd5bb615bc73205e031b2787dd5
Eason Cheng <eason.cheng@xuenn.com>
2020-11-10 16:16:58 +0800

    add get mastercard rate

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from telegram import Update
 from telegram.ext import (
     Updater,
@@ -7,11 +9,30 @@ from telegram.ext import (
 TOKEN = 'YOUR TOKEN HERE'
 
 
-def hello(update: Update, context: CallbackContext):
-    update.message.reply_text('hello {}'.format(update.message.from_user.name))
+def mastercardRate(update: Update, context: CallbackContext):
+    update.message.reply_text('1 USD = {} TWD'.format(getMastercardRate()))
+
+
+def getMastercardRate():
+    headers = {'authority': 'www.mastercard.us',
+                  'pragma': 'no-cache',
+                  'cache-control': 'no-cache',
+                  'accept': 'application/json, text/plain, */*',
+                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                'Chrome/86.0.4240.183 Safari/537.36',
+                  'sec-fetch-site': 'same-origin',
+                  'sec-fetch-mode': 'cors',
+                  'sec-fetch-dest': 'empty',
+                  'referer': 'https://www.mastercard.us/en-us/personal/get-support/convert-currency.html',
+                  'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7'}
+    r = requests.get('https://www.mastercard.us/settlement/currencyrate/fxDate=0000-00-00;transCurr=USD;'
+                     'crdhldBillCurr=TWD;bankFee=0;transAmt=1/conversion-rate',
+                     headers=headers)
+    obj = json.loads(r.text)
+    return obj['data']['conversionRate']
 
 
 updater = Updater(TOKEN)
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('m', mastercardRate))
 updater.start_polling()
 updater.idle()
