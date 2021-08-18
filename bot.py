a02ef44ec1a399e1fbffce30b17c19ddcea6ad85
Eason <eason@seasalt.ai>
2021-08-19 02:22:53 +0800

    fix: dont async await

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
+import requests
 import json
 import locale
 import logging
 import os
 from datetime import datetime, timedelta, timezone
 
-import httpx
 from bs4 import BeautifulSoup
 from cacheout import Cache
 from telegram import Update
@@ -165,144 +165,137 @@ def ask_ust(update: Update, context: CallbackContext):
     update.message.reply_text("Mirror Wallet UST = {} USD".format(get_ust()))
 
 
-async def get_usd_rete_from_3rd():
-    async with httpx.AsyncClient() as client:
-        r = client.get("https://www.bestxrate.com/card/mastercard/usd.html")
-        soup = BeautifulSoup(r.text, "html.parser")
-        masterCardRate = (
-            [
-                i.text
-                for i in soup.select(
-                    "body > div > div:nth-child(3) > div > div > div.panel-body > div:nth-child(4) > div.col-md-10.col-xs-7 > b"
-                )
-            ][0]
-            .replace("\xa0", "")
-            .strip("0")
-        )
-        visaRate = [i.text for i in soup.select("#comparison_huilv_Visa")][0].strip("0")
-        jcbRate = [i.text for i in soup.select("#comparison_huilv_JCB")][0].strip("0")
-        return [masterCardRate, visaRate, jcbRate]
+def get_usd_rete_from_3rd():
+
+    r = requests.get("https://www.bestxrate.com/card/mastercard/usd.html")
+    soup = BeautifulSoup(r.text, "html.parser")
+    masterCardRate = (
+        [
+            i.text
+            for i in soup.select(
+                "body > div > div:nth-child(3) > div > div > div.panel-body > div:nth-child(4) > div.col-md-10.col-xs-7 > b"
+            )
+        ][0]
+        .replace("\xa0", "")
+        .strip("0")
+    )
+    visaRate = [i.text for i in soup.select("#comparison_huilv_Visa")][0].strip("0")
+    jcbRate = [i.text for i in soup.select("#comparison_huilv_JCB")][0].strip("0")
+    return [masterCardRate, visaRate, jcbRate]
 
 
-async def get_usd_rate():
-    usd_rate = await get_usd_rete_from_3rd()
-    esun_usd_rate = await get_usd_rate_esunbank()
+def get_usd_rate():
+    usd_rate = get_usd_rete_from_3rd()
+    esun_usd_rate = get_usd_rate_esunbank()
     return f"USD Rate\n[BUY]\nMastercard: {usd_rate[0]} TWD\nVisa: {usd_rate[1]} TWD\nJCB: {usd_rate[2]} TWD\n[SELL]\n玉山: {esun_usd_rate} TWD"
 
 
-async def get_usd_rate_esunbank():
+def get_usd_rate_esunbank():
     try:
-        async with httpx.AsyncClient() as client:
-            dayStr = (datetime.now(timezone.utc) + timedelta(hours=8)).strftime(
-                "%Y-%m-%d"
-            )
-            timeStr = (datetime.now(timezone.utc) + timedelta(hours=8)).strftime(
-                "%H:%M:%S"
-            )
-            headers = {
-                "Content-Type": "application/json",
-                "Referer": "https://www.esunbank.com.tw/bank/personal/deposit/rate/forex/foreign-exchange-rates",
-                "Host": "www.esunbank.com.tw",
-            }
 
-            r = client.post(
-                "https://www.esunbank.com.tw/bank/Layouts/esunbank/Deposit/DpService.aspx/GetForeignExchageRate",
-                headers=headers,
-                json={"day": dayStr, "time": timeStr},
-            )
+        dayStr = (datetime.now(timezone.utc) + timedelta(hours=8)).strftime("%Y-%m-%d")
+        timeStr = (datetime.now(timezone.utc) + timedelta(hours=8)).strftime("%H:%M:%S")
+        headers = {
+            "Content-Type": "application/json",
+            "Referer": "https://www.esunbank.com.tw/bank/personal/deposit/rate/forex/foreign-exchange-rates",
+            "Host": "www.esunbank.com.tw",
+        }
 
-            obj = json.loads(r.text)
-            if not obj["d"]:
+        r = requests.post(
+            "https://www.esunbank.com.tw/bank/Layouts/esunbank/Deposit/DpService.aspx/GetForeignExchageRate",
+            headers=headers,
+            json={"day": dayStr, "time": timeStr},
+        )
+
+        obj = json.loads(r.text)
+        if not obj["d"]:
+            return -1
+        else:
+            rates = json.loads(obj["d"])
+            # result = rates['Rates'][0]['BBoardRate']
+            result = list(filter(lambda x: x["Name"] == "美元", rates["Rates"]))
+            if not result:
                 return -1
             else:
-                rates = json.loads(obj["d"])
-                # result = rates['Rates'][0]['BBoardRate']
-                result = list(filter(lambda x: x["Name"] == "美元", rates["Rates"]))
-                if not result:
-                    return -1
-                else:
-                    return result[0]["BBoardRate"]
+                return result[0]["BBoardRate"]
     except Exception as e:
         logger.info('except "%s"', e)
         return -1
 
 
-async def get_ace_price():
+def get_ace_price():
     try:
-        async with httpx.AsyncClient() as client:
-            r = client.get(
-                "https://www.ace.io/polarisex/quote/getKline?baseCurrencyId=1&tradeCurrencyId=14&limit=1"
-            )
-            obj = json.loads(r.text)
-            if not obj["attachment"]:
-                return -1
-            else:
-                return float(obj["attachment"][0]["closePrice"])
+        r = requests.get(
+            "https://www.ace.io/polarisex/quote/getKline?baseCurrencyId=1&tradeCurrencyId=14&limit=1"
+        )
+        obj = json.loads(r.text)
+        if not obj["attachment"]:
+            return -1
+        else:
+            return float(obj["attachment"][0]["closePrice"])
     except:
         return -1
 
 
-async def get_bito_price(utc_now: datetime):
+def get_bito_price(utc_now: datetime):
     try:
-        async with httpx.AsyncClient() as client:
-            to_time = int(utc_now.timestamp())
-            from_time = int((utc_now - timedelta(seconds=1800)).timestamp())
-            r = client.get(
-                f"https://api.bitopro.com/v3/trading-history/usdt_twd?resolution=1m&from={from_time}&to={to_time}"
-            )
-            obj = json.loads(r.text)
-            if obj["data"] is None:
-                return -1
-            else:
-                return float(obj["data"][0]["close"])
+        to_time = int(utc_now.timestamp())
+        from_time = int((utc_now - timedelta(seconds=1800)).timestamp())
+        r = requests.get(
+            f"https://api.bitopro.com/v3/trading-history/usdt_twd?resolution=1m&from={from_time}&to={to_time}"
+        )
+        obj = json.loads(r.text)
+        if obj["data"] is None:
+            return -1
+        else:
+            return float(obj["data"][0]["close"])
     except:
         return -1
 
 
-async def get_max_price():
+def get_max_price():
     try:
-        async with httpx.AsyncClient() as client:
-            headers = {
-                "authority": "max.maicoin.com",
-                "pragma": "no-cache",
-                "cache-control": "no-cache",
-                "dnt": "1",
-                "upgrade-insecure-requests": "1",
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,"
-                "*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/86.0.4240.183 Safari/537.36",
-                "sec-fetch-site": "none",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-user": "?1",
-                "sec-fetch-dest": "document",
-                "accept-language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
-            }
-            r = client.get(
-                "https://max.maicoin.com/trades/usdttwd/recent_trades", headers=headers
-            )
-            obj = json.loads(r.text)
-            if not obj["data"]:
-                return -1
-            else:
-                return float(obj["data"][0]["price"])
+        headers = {
+            "authority": "max.maicoin.com",
+            "pragma": "no-cache",
+            "cache-control": "no-cache",
+            "dnt": "1",
+            "upgrade-insecure-requests": "1",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,"
+            "*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/86.0.4240.183 Safari/537.36",
+            "sec-fetch-site": "none",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "sec-fetch-dest": "document",
+            "accept-language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
+        }
+        r = requests.get(
+            "https://max.maicoin.com/trades/usdttwd/recent_trades", headers=headers
+        )
+        obj = json.loads(r.text)
+        if not obj["data"]:
+            return -1
+        else:
+            return float(obj["data"][0]["price"])
     except:
         return -1
 
 
-async def get_usdt():
+def get_usdt():
     utc_now = datetime.now(timezone.utc)
-    bito_price = str(await get_bito_price(utc_now))
+    bito_price = str(get_bito_price(utc_now))
     if bito_price == "-1":
         bito_price = "死了(?)"
     else:
         bito_price = bito_price + " TWD"
-    ace_price = str(await get_ace_price())
+    ace_price = str(get_ace_price())
     if ace_price == "-1":
         ace_price = "死了(?)"
     else:
         ace_price = ace_price + " TWD"
-    max_price = str(await get_max_price())
+    max_price = str(get_max_price())
     if max_price == "-1":
         max_price = "死了(?)"
     else:
@@ -312,34 +305,32 @@ async def get_usdt():
     )
 
 
-async def get_ust():
-    async with httpx.AsyncClient() as client:
-        r = client.get(
-            f"https://api.moonpay.io/v3/currencies/ask_price?cryptoCurrencies=ust&fiatCurrencies=twd,usd&apiKey={MOONPAYKEY}"
-        )
-        obj = json.loads(r.text)
-        return obj["UST"]["USD"]
+def get_ust():
+    r = requests.get(
+        f"https://api.moonpay.io/v3/currencies/ask_price?cryptoCurrencies=ust&fiatCurrencies=twd,usd&apiKey={MOONPAYKEY}"
+    )
+    obj = json.loads(r.text)
+    return obj["UST"]["USD"]
 
 
 @cache.memoize(ttl=3, typed=True)
-async def get_gas():
-    async with httpx.AsyncClient() as client:
-        r = client.get(
-            f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={ETHERSCANKEY}",
-            headers={
-                "host": "etherscan.io",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
-            },
+def get_gas():
+    r = requests.get(
+        f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={ETHERSCANKEY}",
+        headers={
+            "host": "etherscan.io",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+        },
+    )
+    try:
+        obj = json.loads(r.text)
+        return "Low: {}\nAvg: {}\nHigh: {}\nfrom etherscan.io".format(
+            obj["result"]["SafeGasPrice"],
+            obj["result"]["ProposeGasPrice"],
+            obj["result"]["FastGasPrice"],
         )
-        try:
-            obj = json.loads(r.text)
-            return "Low: {}\nAvg: {}\nHigh: {}\nfrom etherscan.io".format(
-                obj["result"]["SafeGasPrice"],
-                obj["result"]["ProposeGasPrice"],
-                obj["result"]["FastGasPrice"],
-            )
-        except:
-            logger.info(r.text)
+    except:
+        logger.info(r.text)
 
 
 def isfloat(value):
